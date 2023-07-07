12bd7831508d16eaa4554c3dbcbd9944f5fe9630
Muhammad Inal Zaelani <inalzaelani2806@gmail.com>
2023-07-07 14:25:08 +0700

    [ADD] Upload File in Club and Race

--- RunGroopWebApp/Program.cs
+++ RunGroopWebApp/Program.cs
@@ -1,14 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
+using RunGroopWebApp.Helpers;
 using RunGroopWebApp.Interface;
 using RunGroopWebApp.Repository;
+using RunGroopWebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddScoped<IClubRepository, ClubRepository>();
+
 builder.Services.AddScoped<IRaceRepository, RaceRepository>();
+
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
